250e0585aa8ce042deec12fc48a3912509b287ad
Arsheel Sheikh <sheikharsheel@gmail.com>
2023-02-10 03:00:10 +0530

    updated App.js

--- userdashboard/src/App.js
+++ userdashboard/src/App.js
@@ -1,56 +1,78 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState, useEffect } from 'react';
-import { ethers } from 'ethers';
-import './App.css';
-import Header from './components/Header';
-import FileUpload from './components/FileUpload';
+import SmartContract from "./artifacts/contracts/SmartContract.sol/Upload.json"
+import "./App.css";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import FileUpload from "./Components/FileUpload";
+import Display from "./Components/Display";
+import Modal from "./Components/Modal";
+
 
 function App() {
-  const [wallet, setWallet] = useState('');
+  const [account, setAccount] = useState("");
   const [contract, setContract] = useState(null);
   const [provider, setProvider] = useState(null);
+  const [modalOpen, setModalOpen] = useState(false);
 
   useEffect(() => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
 
     const loadProvider = async () => {
       if (provider) {
-        window.ethereum.on('chainChanged', () => {
+        window.ethereum.on("chainChanged", () => {
           window.location.reload();
         });
 
-        window.ethereum.on('accountsChanged', () => {
+        window.ethereum.on("accountsChanged", () => {
           window.location.reload();
         });
-        await provider.send('eth_requestAccounts', []);
+        await provider.send("eth_requestAccounts", []);
         const signer = provider.getSigner();
         const address = await signer.getAddress();
-        setWallet(address);
+        setAccount(address);
+        let contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
+        const contract = new ethers.Contract(
+          contractAddress,
+          // eslint-disable-next-line no-undef
+          Upload.abi,
+          signer
+        );
+        //console.log(contract);
         setContract(contract);
         setProvider(provider);
       } else {
-        console.error('Metamask is not installed');
+        console.error("Metamask is not installed");
       }
     };
     provider && loadProvider();
   }, []);
   return (
     <>
-      <Header wallet={wallet} />
+      {!modalOpen && (
+        <button className="share" onClick={() => setModalOpen(true)}>
+          Share
+        </button>
+      )}
+      {modalOpen && (
+        <Modal setModalOpen={setModalOpen} contract={contract}></Modal>
+      )}
 
-      <div className='App'>
-        <div className='container'>
-          <div className='row'>
-            <h1 className='text-center my-5 text-gradient'>
-              The Power of Decentralization Meets Ease of Use with D-Drop
-            </h1>
-            <div className='bg'></div>
-            <div className='bg bg2'></div>
-            <div className='bg bg3'></div>
-            <FileUpload wallet={wallet} provider={provider} contract={contract} />
-          </div>
-        </div>
+      <div className="App">
+        <h1 style={{ color: "white" }}>D-Drop</h1>
+        <div class="bg"></div>
+        <div class="bg bg2"></div>
+        <div class="bg bg3"></div>
+
+        <p style={{ color: "white" }}>
+          Account : {account ? account : "Not connected"}
+        </p>
+
+        <FileUpload
+          account={account}
+          provider={provider}
+          contract={contract}
+        ></FileUpload>
+        
       </div>
     </>
   );
