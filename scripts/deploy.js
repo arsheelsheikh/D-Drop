5103429a849c10bf26c1c1bd4fcc6b11d0eb83e6
Arsheel Sheikh <sheikharsheel@gmail.com>
2023-02-09 17:14:45 +0530

    Add files via upload

---
+++ scripts/deploy.js
@@ -0,0 +1,17 @@
+// this is deploying script
+
+const hre = require("hardhat");
+
+async function main() {
+  const Upload = await hre.ethers.getContractFactory("Upload");
+  const upload = await Upload.deploy();
+
+  await upload.deployed();
+
+  console.log("Library deployed to:", upload.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
